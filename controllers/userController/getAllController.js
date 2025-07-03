90fe7db593c25cd0143eb39882a6851591af7f2a
Atharva Harane <157909133+Atharva12072004@users.noreply.github.com>
2025-07-03 18:29:04 +0530

    Update getAllController.js

--- controllers/userController/getAllController.js
+++ controllers/userController/getAllController.js
@@ -1,13 +1,22 @@
 const userModel = require("../../models/userSchema");
-const getAll = async (req, res) =>{
-    try {
-        const users = await userModel.find({});
-        if (!users) {
-          return res.status(404).json({ msg: "Not Found" }); // 404 Not Found
-        }
-        return res.json(users);
-      } catch (err) {
-        return res.status(500).json({ msg: "Internal Server Error" }); // 500 Internal Server Error
-      }
+
+const getAll = async (req, res) => {
+
+
+  try {
+
+    if(req.user_rank >= 3){
+      return res.status(200).json([]);
+    }
+
+    const users = await userModel.find({ rank: 1 }); // Only fetch users with rank = 1
+    if (!users.length) {
+      return res.status(404).json({ msg: "No Users" }); // 404 Not Found
+    }
+    return res.json(users);
+  } catch (err) {
+    return res.status(500).json({ msg: "Internal Server Error" }); // 500 Internal Server Error
+  }
 }
-module.exports = { getAll }
+
+module.exports = { getAll };
