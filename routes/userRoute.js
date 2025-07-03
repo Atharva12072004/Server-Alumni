3268d6995de3c89e3be6ea33eca8e036e20149c3
Atharva Harane <157909133+Atharva12072004@users.noreply.github.com>
2025-07-03 18:12:10 +0530

    Update userRoute.js

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,33 +1,28 @@
-// server/routes/userRoute.js
-
 const express = require("express");
-const router = express.Router();
+const router  = express.Router();
 
-// 1️⃣ Import your auth middleware
-const { jwtAuth } = require("../middlewares/jwtAuth");
-
-// 2️⃣ Import all controllers
+const { jwtAuth, alumniVerify } = require("../middlewares/jwtAuth");
 const { signup }      = require("../controllers/userController/signupController");
+const { verify }      = require("../controllers/userController/otpController");
 const { login }       = require("../controllers/userController/loginController");
 const { get }         = require("../controllers/userController/getController");
+const { getByToken }  = require("../controllers/userController/getByTokenController");
+const { getPending }  = require("../controllers/userController/getPendingController");
 const { getAll }      = require("../controllers/userController/getAllController");
 const { edit }        = require("../controllers/userController/editController");
 const { remove }      = require("../controllers/userController/deleteController");
-const { verify }      = require("../controllers/userController/otpController");
-const { getByToken }  = require("../controllers/userController/getByTokenController");
-const { getPending }  = require("../controllers/userController/getPendingController");
 
-// 3️⃣ Public routes
-router.post("/signup", signup);              // User Signup
-router.post("/signup/verify", verify);       // OTP Verification
-router.post("/login", login);                // User Login
-router.post("/get", get);                    // Get User By ID (body)
+// Public
+router.post("/signup",           signup);
+router.post("/signup/verify",    verify);
+router.post("/login",            login);
+router.post("/get",              get);
 
-// 4️⃣ Protected routes (require JWT)
-router.get("/get", jwtAuth, getByToken);     // Get User By Token
-router.get("/getPending", jwtAuth, getPending);   // Admin: Get unverified alumni
-router.get("/getAll", jwtAuth, getAll);      // Admin: Get all users
-router.put("/edit", jwtAuth, edit);          // Edit user (approve, profile update, etc.)
-router.post("/delete", jwtAuth, remove);     // Delete user
+// Protected
+router.get( "/get",           jwtAuth,      getByToken);
+router.get( "/getPending",    jwtAuth,      getPending);
+router.get( "/getAll",        jwtAuth,      getAll);
+router.put( "/edit",          jwtAuth,alumniVerify, edit);
+router.post("/delete",        jwtAuth,      remove);
 
 module.exports = router;
