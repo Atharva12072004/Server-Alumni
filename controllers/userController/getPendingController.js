4100ed53186684b45bb06f988cb8b90109a447db
Atharva Harane <157909133+Atharva12072004@users.noreply.github.com>
2025-07-03 18:12:49 +0530

    Update getPendingController.js

--- controllers/userController/getPendingController.js
+++ controllers/userController/getPendingController.js
@@ -1,13 +1,16 @@
 const userModel = require("../../models/userSchema");
-const getPending = async (req, res) =>{
-    try {
-        const users = await userModel.find({verificationStatus:0});
-        if (!users) {
-          return res.status(404).json({ msg: "Not Found" }); // 404 Not Found
-        }
-        return res.json(users);
-      } catch (err) {
-        return res.status(500).json({ msg: "Internal Server Error" }); // 500 Internal Server Error
-      }
-}
-module.exports = { getPending }
+
+const getPending = async (req, res) => {
+  try {
+    const users = await userModel.find({ verificationStatus: 0 });
+    if (!users.length) {
+      return res.status(404).json({ msg: "No pending users" });
+    }
+    return res.json(users);
+  } catch (err) {
+    console.error("getPending Error:", err);
+    return res.status(500).json({ msg: "Internal Server Error" });
+  }
+};
+
+module.exports = { getPending };
