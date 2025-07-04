8cc1658f950efbc507bdc1d70c0f4532d09bff4c
Atharva Harane <157909133+Atharva12072004@users.noreply.github.com>
2025-07-05 02:02:21 +0530

    Delete routes directory

--- routes/alumniRoute.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router();
-const { alumniVerify } = require('../middlewares/jwtAuth');
-const { getAll } = require('../controllers/alumniController/getAllController');
-const {getRank} = require('../middlewares/getRank');
-
-
-router.get("/getAll", getRank, getAll); // Get All Users
-module.exports = router;
